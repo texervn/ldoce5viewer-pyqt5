f2f3f674bbc3dd77a67a9e09d59ce57f3318a209
texervn <nvhkph@gmail.com>
2018-11-09 13:22:42 -0600

    Update main.py

--- ldoce5viewer/qtgui/main.py
+++ ldoce5viewer/qtgui/main.py
@@ -26,6 +26,7 @@ from PyQt5.QtNetwork import *
 from PyQt5.QtWebKit import *
 from PyQt5.QtWebKitWidgets import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtPrintSupport import QPrinter, QPrintDialog, QPrintPreviewDialog
 
 from .. import fulltext
 from .. import incremental
